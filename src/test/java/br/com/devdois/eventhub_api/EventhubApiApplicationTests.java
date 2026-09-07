50ffbc0b01064936612c18cafbcb41b5589dabad
niltinfilho <niltonfilho115@gmail.com>
2026-09-07 16:39:16 -0300

    first commit: create project

---
+++ src/test/java/br/com/devdois/eventhub_api/EventhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devdois.eventhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
